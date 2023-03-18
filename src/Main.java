395b681b2dea1d043918efa5bc2e83553be108ee
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 14:05:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en metod som roterar alla element i en array ett steg till höger
+        // (sista elementet skall hamna på index 0)
+
+
+    }
+}
