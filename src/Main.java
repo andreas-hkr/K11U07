b0366b2a648b637bd4da06ddd24b042aa2727d24
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 14:21:08 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
+    static void rotate(int[] arr) {
+        int temp = arr[arr.length-1];
+
+        for (int i= arr.length-1; i >= 1; i--) {
+            arr[i] = arr[i-1];
+        }
+
+        arr[0] = temp;
+    }
+
     public static void main(String[] args) {
         // Skriv en metod som roterar alla element i en array ett steg till höger
         // (sista elementet skall hamna på index 0)
 
-
+        int[] arr = {1, 2, 3, 4};
+        rotate(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
